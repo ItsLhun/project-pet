731bb22c9067a40e7c814a13482d5fb1fa73d7f8
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-09-13 11:24:43 +0200

    add email validation to sign up and add frontend validation

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const validateEmail = require('./../helperJS/validateEmail');
 
 const schema = new mongoose.Schema({
   name: {
@@ -9,14 +10,21 @@ const schema = new mongoose.Schema({
   },
   username: {
     type: String,
+    minlength: 3,
     required: true,
-    trim: true
+    trim: true,
+    unique: true
   },
   email: {
     type: String,
     required: true,
     lowercase: true,
-    trim: true
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validateEmail,
+      message: 'Please enter a valid email address.'
+    }
   },
   passwordHashAndSalt: {
     type: String
