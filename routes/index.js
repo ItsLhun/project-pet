52c0dc8099ebf583581cda5efe158427aedaa299
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-09-13 13:36:12 +0200

    rename private to user-profile and add form and routes for editing profile details

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,27 @@
 const express = require('express');
 const router = express.Router();
 const routeGuard = require('./../middleware/route-guard');
+const User = require('./../models/user');
 
 router.get('/', (req, res, next) => {
   res.render('home', { title: 'Hello World!' });
 });
 
-router.get('/my-account', routeGuard, (req, res, next) => {
-  res.render('my-account');
+router.get('/user-profile', routeGuard, (req, res, next) => {
+  res.render('user-profile/detail');
+});
+
+router.get('/user-profile/edit', routeGuard, (req, res, next) => {
+  res.render('user-profile/edit');
+});
+
+router.post('/user-profile/edit', routeGuard, (req, res, next) => {
+  const { id } = req.user;
+  const { name, username, email } = req.body;
+
+  User.findByIdAndUpdate(id, { name, username, email })
+    .then(() => res.redirect('/user-profile'))
+    .catch((error) => next(error));
 });
 
 module.exports = router;
