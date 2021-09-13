b5085c54ab44182f46ba6ad951f3ce025f6c0c50
itslhun <nahuelghastin@outlook.com>
2021-09-13 23:34:59 +0200

    integrate sidebar navigation

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,29 @@
-window.addEventListener('load', () => {
-  console.log('Ironmaker app started successfully!');
-}, false);
+window.addEventListener(
+  'load',
+  () => {
+    console.log('Ironmaker app started successfully!');
+  },
+  false
+);
+
+const sideMenuInput = document.querySelector('#side-menu-input');
+const sideMenuToggle = document.querySelector('#menu-toggle');
+const bodyElement = document.querySelector('body');
+const sideMenu = document.querySelector('#sidebar-menu');
+//const firstSectionElement = document.querySelector('section:first-of-type');
+
+let sideStatus = sideMenu.style.transform;
+
+sideMenuInput.addEventListener('change', () => {
+  sideStatus = sideMenu.style.transform;
+  console.log('changed');
+  if (sideStatus === 'none') {
+    sideMenu.style.transform = 'translate(100%, 0)';
+    bodyElement.classList.remove('no-scroll');
+    sideMenu.classList.add('sidebar-no-show');
+  } else {
+    sideMenu.style.transform = 'none';
+    sideMenu.classList.remove('sidebar-no-show');
+    bodyElement.classList.add('no-scroll');
+  }
+});
