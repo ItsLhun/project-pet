200d5988127f1a8a91691ca0366b8cd2151345a4
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-09-14 17:14:39 +0200

    fix broken dest path for sass middleware

--- config/sass.js
+++ config/sass.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   src: path.join('styles'),
-  dest: path.join(__dirname, 'public/styles'),
+  dest: path.join(__dirname, '../public/styles'),
   prefix: '/styles',
   outputStyle:
     process.env.NODE_ENV === 'development' ? 'expanded' : 'compressed',
