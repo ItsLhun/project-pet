291c27b658b324988bdfdf7eb610a6a75a03320d
itslhun <nahuelghastin@outlook.com>
2021-09-14 18:51:59 +0200

    add guards to /sign-in to prevent signed in users to access it

--- routes/authentication.js
+++ routes/authentication.js
@@ -53,32 +53,33 @@ router.get('/sign-in', (req, res, next) => {
 });
 
 router.post('/sign-in', (req, res, next) => {
-  let user;
-  const { emailOrUsername, password } = req.body;
-
-  User.findOne()
-    .or([{ username: emailOrUsername }, { email: emailOrUsername }])
-    .then((document) => {
-      if (!document) {
-        return Promise.reject(
-          new Error("There's no user with that email or username.")
-        );
-      } else {
-        user = document;
-        return bcryptjs.compare(password, user.passwordHashAndSalt);
-      }
-    })
-    .then((result) => {
-      if (result) {
-        req.session.userId = user._id;
-        res.redirect('/');
-      } else {
-        return Promise.reject(new Error('Wrong password.'));
-      }
-    })
-    .catch((error) => {
-      next(error);
-    });
+  if (!req.session.userId) {
+    let user;
+    const { emailOrUsername, password } = req.body;
+    User.findOne()
+      .or([{ username: emailOrUsername }, { email: emailOrUsername }])
+      .then((document) => {
+        if (!document) {
+          return Promise.reject(
+            new Error("There's no user with that email or username.")
+          );
+        } else {
+          user = document;
+          return bcryptjs.compare(password, user.passwordHashAndSalt);
+        }
+      })
+      .then((result) => {
+        if (result) {
+          req.session.userId = user._id;
+          res.redirect('/');
+        } else {
+          return Promise.reject(new Error('Wrong password.'));
+        }
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
 });
 
 router.post('/sign-out', (req, res, next) => {
