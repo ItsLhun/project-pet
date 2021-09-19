077fa2d0e9554c514c3156046672b9a45497ef1a
itslhun <nahuelghastin@outlook.com>
2021-09-19 11:54:04 +0200

    fix sidebar behavior when clicking on body target

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -1,8 +1,8 @@
 const sideMenuInput = document.querySelector('#side-menu-input');
 const sideMenuToggle = document.querySelector('#menu-toggle');
 const bodyElement = document.querySelector('body');
+const htmlElement = document.querySelector('html');
 const sideMenu = document.querySelector('#sidebar-menu');
-//const firstSectionElement = document.querySelector('section:first-of-type');
 
 let sideStatus = sideMenu?.style?.transform;
 
@@ -10,12 +10,25 @@ sideMenuInput.addEventListener('change', () => {
   sideStatus = sideMenu.style.transform;
   console.log('changed');
   if (sideStatus === 'none') {
+    console.log(sideMenuInput.checked);
     sideMenu.style.transform = 'translate(100%, 0)';
     bodyElement.classList.remove('no-scroll');
+    htmlElement.classList.remove('no-scroll');
     sideMenu.classList.add('sidebar-no-show');
   } else {
     sideMenu.style.transform = 'none';
     sideMenu.classList.remove('sidebar-no-show');
     bodyElement.classList.add('no-scroll');
+    htmlElement.classList.add('no-scroll');
+  }
+});
+bodyElement.addEventListener('click', (e) => {
+  if (bodyElement === e.target) {
+    console.log('blicked body');
+    sideMenuInput.checked = false;
+    sideMenu.classList.add('sidebar-no-show');
+    sideMenu.style.transform = 'translate(100%, 0)';
+    bodyElement.classList.remove('no-scroll');
+    htmlElement.classList.remove('no-scroll');
   }
 });
