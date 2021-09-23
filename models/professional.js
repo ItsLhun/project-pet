84ac6d5428cca55d09f1f67ed8d214bd53851d6c
itslhun <nahuelghastin@outlook.com>
2021-09-23 20:29:20 +0200

    create professional model and login view, revamp sign up form

---
+++ models/professional.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const validateEmail = require('../helperJS/validateEmail');
+const capitalizeWords = require('../helperJS/capitalize');
+
+const profSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    set: capitalizeWords,
+    required: true
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    set: capitalizeWords,
+    required: true
+  },
+  username: {
+    type: String,
+    minlength: 3,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validateEmail,
+      message: 'Please enter a valid email address.'
+    }
+  },
+  type: {
+    type: String,
+    enum: ['Veterinarian', 'Walker', 'Pet Sitter', 'Groomer', 'Other'],
+    required: true
+  },
+  profilePicture: {
+    type: String,
+    default: '/images/default-profile-picture.png'
+  },
+  passwordHashAndSalt: {
+    type: String
+  }
+});
+
+const Professional = mongoose.model('Professional', profSchema);
+
+module.exports = Professional;
--- routes/authentication.js
+++ routes/authentication.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('./../models/user');
+const Professional = require('./../models/professional');
+
 const passwordValidator = require('./../middleware/password-validator');
 
 const router = express.Router();
@@ -37,7 +39,7 @@ router.post(
   }
 );
 
-router.get('/sign-in', (req, res, next) => {
+router.get('/user-sign-in', (req, res, next) => {
   if (req.user) {
     res.render('dashboard');
   } else {
@@ -45,7 +47,7 @@ router.get('/sign-in', (req, res, next) => {
   }
 });
 
-router.post('/sign-in', (req, res, next) => {
+router.post('/user-sign-in', (req, res, next) => {
   if (!req.user) {
     let user;
     const { emailOrUsername, password } = req.body;
@@ -73,7 +75,50 @@ router.post('/sign-in', (req, res, next) => {
           res.redirect('/authentication/sign-in');
         } else if (error.message === 'NO_USER') {
           req.flash('userError', 'No user found with the username/email.');
+          res.redirect('/authentication/user-sign-in');
+        }
+        next(error);
+      });
+  }
+});
+
+router.get('/prof-sign-in', (req, res, next) => {
+  if (req.user) {
+    res.render('dashboard');
+  } else {
+    res.render('authentication/prof-sign-in', { message: req.flash() });
+  }
+});
+
+router.post('/prof-sign-in', (req, res, next) => {
+  if (!req.user) {
+    let user;
+    const { emailOrUsername, password } = req.body;
+    Professional.findOne()
+      .or([{ username: emailOrUsername }, { email: emailOrUsername }])
+      .then((document) => {
+        if (!document) {
+          return Promise.reject(new Error('NO_USER'));
+        } else {
+          user = document;
+          return bcryptjs.compare(password, user.passwordHashAndSalt);
+        }
+      })
+      .then((result) => {
+        if (result) {
+          req.session.userId = user._id;
+          res.redirect('/');
+        } else {
+          return Promise.reject(new Error('WRONG_PASSWORD'));
+        }
+      })
+      .catch((error) => {
+        if (error.message === 'WRONG_PASSWORD') {
+          req.flash('passwordError', 'Wrong password.');
           res.redirect('/authentication/sign-in');
+        } else if (error.message === 'NO_USER') {
+          req.flash('userError', 'No user found with the username/email.');
+          res.redirect('/authentication/prof-sign-in');
         }
         next(error);
       });
