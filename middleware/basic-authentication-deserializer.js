d3b5a13bca21f168c5546f61b8f3e0e324885bb2
itslhun <nahuelghastin@outlook.com>
2021-09-24 21:27:28 +0200

    correctly set user session when professional

--- middleware/basic-authentication-deserializer.js
+++ middleware/basic-authentication-deserializer.js
@@ -1,10 +1,21 @@
 'use strict';
 
 const User = require('./../models/user');
+const Professional = require('./../models/professional');
 
 module.exports = (req, res, next) => {
   const userId = req.session.userId;
-  if (userId) {
+  const type = req.session.userType;
+  if (type && userId) {
+    Professional.findById(userId)
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((error) => {
+        next(error);
+      });
+  } else if (userId) {
     User.findById(userId)
       .then((user) => {
         req.user = user;
--- middleware/bind-user-to-view-locals.js
+++ middleware/bind-user-to-view-locals.js
@@ -2,5 +2,7 @@
 
 module.exports = (req, res, next) => {
   res.locals.user = req.user;
+  console.log('requsermiddle', req.user);
+  // res.locals.type = req.user.type;
   next();
 };
