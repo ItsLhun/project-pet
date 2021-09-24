f1b6fafa23b0cd444e0ac50a368583a2e0ead29b
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-09-24 21:51:15 +0200

    finalize message refactor

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -6,14 +6,15 @@ for (let i = 0; i < messages.length; i++) {
   message.addEventListener('click', () => {
     const collapsible = message.querySelector('.collapsible');
     collapsible.classList.toggle('active');
+
     let content = collapsible.nextElementSibling;
-    console.log(content);
+
     if (content.style.display === 'block') {
       content.style.display = 'none';
     } else {
       content.style.display = 'block';
     }
-    console.log('hi');
+
     axios
       .post(`http://localhost:3000/message/read/${message.id}`)
       .then((res) => console.log('Success'))
