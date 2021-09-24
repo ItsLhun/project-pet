a12799cd4077742c37ba5e70553a00398b42c06e
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-09-24 21:40:02 +0200

    fix collapsible messages to work for multiple messages at the same time

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -1,9 +1,19 @@
-const messages = document.getElementsByClassName('message');
+const messages = document.getElementsByClassName('user-message');
 
 for (let i = 0; i < messages.length; i++) {
   let message = messages[i];
 
   message.addEventListener('click', () => {
+    const collapsible = message.querySelector('.collapsible');
+    collapsible.classList.toggle('active');
+    let content = collapsible.nextElementSibling;
+    console.log(content);
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+    console.log('hi');
     axios
       .post(`http://localhost:3000/message/read/${message.id}`)
       .then((res) => console.log('Success'))
