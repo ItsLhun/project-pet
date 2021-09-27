33ac16bd663d85399b29e4f5d9f653f55a1c1eda
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-09-27 12:45:41 +0200

    add backend logic for ownership transfer

--- routes/message.js
+++ routes/message.js
@@ -40,21 +40,43 @@ router.post('/authorize/:petId', routeGuard, (req, res, next) => {
     .catch((error) => next(error));
 });
 
+router.post('/transfer-ownership/:petId', routeGuard, (req, res, next) => {
+  const { petId } = req.params;
+  const { username, note } = req.body;
+
+  User.findOne({ username })
+    .then((user) => {
+      return Message.create({
+        from: req.user.id,
+        to: user.id,
+        type: 'Pet Access Invitation',
+        pet: petId,
+        note
+      });
+    })
+    .then(() => res.redirect(`/pet/${petId}`))
+    .catch((error) => next(error));
+});
+
 router.post('/respond/:id', routeGuard, (req, res, next) => {
   const status = req.body.accept || req.body.decline;
   const { id } = req.params;
 
   Message.findByIdAndUpdate(id, { status, confirmed: true }, { new: true })
     .then((updatedMessage) => {
-      switch (updatedMessage.type) {
-        case 'Pet Access Invitation':
-          return status === 'Accepted'
-            ? Pet.findByIdAndUpdate(updatedMessage.pet, {
-                $push: { authorized: req.user.id }
-              })
-            : null;
-        case 'Vet Appointment Request':
-          break;
+      if (status === 'Accepted') {
+        switch (updatedMessage.type) {
+          case 'Pet Access Invitation':
+            return Pet.findByIdAndUpdate(updatedMessage.pet, {
+              $push: { authorized: req.user.id }
+            });
+          case 'Vet Appointment Request':
+            break;
+          case 'Ownership Transfer Request':
+            return Pet.findByIdAndUpdate(updatedMessage.pet, {
+              owner: req.user.id
+            });
+        }
       }
     })
     .then(() => res.redirect('/user/messages'))
