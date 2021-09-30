fa07b613d8a6b0c7746226624dde505590ebd894
itslhun <nahuelghastin@outlook.com>
2021-09-30 10:32:27 +0200

    fix refreshing error on profile

--- public/scripts/edit-user-settings.js
+++ public/scripts/edit-user-settings.js
@@ -1,10 +1,12 @@
 const colorInputs = document.getElementsByClassName('event-color');
 const eventColorForm = document.getElementById('event-color-form');
 
-document.addEventListener('change', () => {
-  for (let i = 0; i < colorInputs.length; i++) {
-    colorInputs[i].parentNode.style.backgroundColor = colorInputs[i].value;
-  }
+for (let i = 0; i < colorInputs.length; i++) {
+  colorInputs[i].addEventListener('change', () => {
+    for (let i = 0; i < colorInputs.length; i++) {
+      colorInputs[i].parentNode.style.backgroundColor = colorInputs[i].value;
+    }
 
-  eventColorForm.submit();
-});
+    eventColorForm.submit();
+  });
+}
